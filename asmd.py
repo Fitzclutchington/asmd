0a15984edf34b4764289f4208e2d4d13938170b3
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2017-08-17 17:34:34 -0400

    added minimum height and width for figures, also removed unecessary label

--- asmd.py
+++ asmd.py
@@ -82,11 +82,10 @@ class Granule(object):
     def _compute_overlay(self, acspo_mask, spt_mask):
         overlay = np.zeros(acspo_mask.shape).astype(np.uint8)
         overlay[(acspo_mask==0) & (spt_mask==0)] = 0 
-        overlay[(acspo_mask==0) & (spt_mask==3)] = 1
-        overlay[(acspo_mask==0) & (spt_mask==4)] = 2
-        overlay[(acspo_mask==1) & (spt_mask==0)] = 3 
-        overlay[(acspo_mask==1) & (spt_mask==3)] = 4
-        overlay[(acspo_mask==1) & (spt_mask==4)] = 5 
+        overlay[(acspo_mask==0) & (spt_mask==4)] = 1
+        overlay[(acspo_mask==1) & (spt_mask==0)] = 2 
+        overlay[(acspo_mask==1) & (spt_mask==3)] = 3
+        overlay[(acspo_mask==1) & (spt_mask==4)] = 4 
     	return overlay
 
     def _compute_gradient(self, sst):
@@ -126,7 +125,7 @@ class Granule(object):
     	layers['spt'] = spt_mask  	
     	layers['overlay'] = self._compute_overlay(acspo_mask,spt_mask)
     	layers['gradient'] = self._compute_gradient(layers['sst'])
-        layers['questionable'] = ((layers['overlay']==3) | (layers['overlay']== 5))
+        layers['questionable'] = ((layers['overlay']==2) | (layers['overlay']== 4))
 
         if self.day:
             layers['albedo'] = self._read_var('albedo_chM7')
@@ -143,6 +142,78 @@ def generate_saveloc(up, down, left, right, save_folder):
     print crop_name
     return os.path.join(save_folder,crop_name)
 
+def add_offset(ind, offset ,  alt, max_val):
+    if not max_val:
+        if(ind >= offset):
+            ind -= offset
+        else:
+            ind = alt
+    else:
+        if(ind < (max_val - offset)):
+            ind += offset
+        else:
+            ind = alt
+    return ind
+
+def generate_crop(mask):
+    min_height = 400
+    min_width = 400
+
+    padding = 100
+    inds_row, inds_col =  np.where(mask)
+    
+    inds_left = np.min(inds_col)
+    inds_right =np.max(inds_col)
+    inds_up = np.min(inds_row)
+    inds_down = np.max(inds_row)
+
+    height_crop = inds_down - inds_up
+    width_crop = inds_right - inds_left
+
+    # add padding, check boundaries
+    if width_crop >= min_width:
+        inds_left = add_offset(inds_left, padding, 0, 0)
+        inds_right = add_offset(inds_right, padding, granule.width-1, granule.width)
+
+
+    else:
+    
+        # add padding to the left
+        width_remaining = min_width - width_crop
+        inds_left = add_offset(inds_left, width_remaining/2, 0, 0)
+   
+        # add padding to the right
+        width_remaining = min_width - (inds_right - inds_left)
+        inds_right = add_offset(inds_right, width_remaining, granule.width-1, granule.width)
+     
+        # add remaining pdding to left if right was greater than width of matrix
+        width_remaining = min_width - (inds_right - inds_left)
+        inds_left = add_offset(inds_left, width_remaining, 0, 0)
+      
+
+    if height_crop >= min_height:
+        inds_up = add_offset(inds_up, padding, 0, 0)
+        inds_down = add_offset(inds_down, padding, granule.height-1, granule.height)
+
+
+    else:
+
+        # add padding to the top
+        height_remaining = min_height - height_crop
+        inds_up = add_offset(inds_up, height_remaining/2, 0, 0)
+        
+        # add padding to the bottom
+        height_remaining = min_height - (inds_down - inds_up)
+        inds_down = add_offset(inds_down, height_remaining, granule.height-1, granule.height)
+        
+        # add remaining pdding to top if bottom was greater than height of matrix
+        height_remaining = min_height - (inds_down - inds_up)
+        inds_up = add_offset(inds_up,height_remaining,0, 0)
+    
+
+    return (inds_up, inds_down, inds_left, inds_right)
+
+
 
 
 if len(sys.argv) < 3:
@@ -157,7 +228,7 @@ granule.generate_save_folder(save_path)
 layers = granule.get_layers()
 
 #create custom colorbar for overlay
-colors = ['#000000', '#4DAF4A', '#999999', '#E41A1C', '#377EB8','#FF7F00']
+colors = ['#000000', '#999999', '#E41A1C', '#377EB8','#FF7F00']
 cmap_overlay = cl.ListedColormap(colors)
 
 #compute connected components
@@ -165,49 +236,20 @@ labels, n_lbls = measure.label(layers['questionable'],background=0, return_num=T
 
 #compute centers of clusters and left right up and down of cluster and save to file
 centers = {}
-inds_left = {}
-inds_right = {}
-inds_down = {}
-inds_up = {}
+
 
 padding = 50
 for lbl in range(n_lbls):
     mask = labels == lbl
     centers[lbl] = ndimage.measurements.center_of_mass(labels,mask)
-    inds_row, inds_col =  np.where(mask)
     
-    inds_left[lbl] = np.min(inds_col)
-    inds_right[lbl] =np.max(inds_col)
-    inds_up[lbl] = np.min(inds_row)
-    inds_down[lbl] = np.max(inds_row)
+    inds_up, inds_down, inds_left, inds_right = generate_crop(mask)
 
+    crop = np.s_[inds_up:inds_down, inds_left:inds_right]
+    saveloc = generate_saveloc(inds_up,inds_down, inds_left,inds_right, granule.save_folder)
     
-    # add padding, check boundaries
-    if(inds_left[lbl] >= padding):
-        inds_left[lbl] -= padding
-    else:
-        inds_left[lbl] = 0 
-
-    if(inds_right[lbl] < granule.width - padding):
-        inds_right[lbl] += padding
-    else:
-        inds_right[lbl] = granule.width - 1 
-
-    if(inds_up[lbl] >= padding):
-        inds_up[lbl] -= padding
-    else:
-        inds_up[lbl] = 0 
-
-    if(inds_down[lbl] < granule.height - padding):
-        inds_down[lbl] += padding
-    else:
-        inds_down[lbl] = granule.height - 1 
-    
-    crop = np.s_[inds_up[lbl]:inds_down[lbl], inds_left[lbl]:inds_right[lbl]]
-    saveloc = generate_saveloc(inds_up[lbl],inds_down[lbl], inds_left[lbl],inds_right[lbl], granule.save_folder)
-    
-    fig = plt.figure(figsize=(10,6))
-    fig.suptitle("Diagnostic at Crop %d : %d, %d : %d" % (inds_up[lbl],inds_down[lbl], inds_left[lbl],inds_right[lbl]))
+    fig = plt.figure(figsize=(11,8))
+    fig.suptitle("Diagnostic at Crop %d : %d, %d : %d" % (inds_up,inds_down, inds_left,inds_right))
 
     plt.ticklabel_format(useOffset=False)
     vmin = 271.15 if np.nanmin(layers['sst'][crop][layers['spt'][crop]!=3]) < 271.15 else np.nanmin(layers['sst'][crop][layers['spt'][crop]!=3])
@@ -216,6 +258,7 @@ for lbl in range(n_lbls):
     if abs(vmin-vmax) < 1:
         vmin = vmin - 1
         vmax = vmax + 1
+
     ax1 = plt.subplot(221)
     ax1.set_title("Sea Surface Temperature", fontsize=10)
     ax1.axis('off')
@@ -247,19 +290,16 @@ for lbl in range(n_lbls):
     cax1 = div1.append_axes("right", size="5%", pad=0.05)
     cbar1 = plt.colorbar(img1, cax=cax1)
 
-    norm = cl.BoundaryNorm(np.arange(0,6,1),cmap_overlay.N )
-    ticklabels = ['Agreed Clear' ,'SPT Cloud','Clear Front','SPT Clear', 'Agreed Cloud','Cloud Front']
+    ticklabels = ['Agreed Clear' ,'Clear Front','SPT Clear', 'Agreed Cloud','Cloud Front']
     ax2 = plt.subplot(224)
     ax2.set_title('Questionable', fontsize=10)
     ax2.axis('off')
-    img1 = ax2.imshow(layers['overlay'][crop],cmap=cmap_overlay,vmin=-0.5,vmax=5.5)
+    img1 = ax2.imshow(layers['overlay'][crop],cmap=cmap_overlay,vmin=-0.5,vmax=4.5)
     ax2.imshow(layers['land'][crop])
     div1 = make_axes_locatable(ax2)
     cax1 = div1.append_axes("right", size="5%", pad=0.05)
-    cbar1 = plt.colorbar(img1, cax=cax1,ticks=np.linspace(0,5,6))
+    cbar1 = plt.colorbar(img1, cax=cax1,ticks=np.linspace(0,4,5))
     cbar1.set_ticklabels(ticklabels)
 
-    #plt.tight_layout()
-    plt.savefig(saveloc, bbox_layout='tight')
     plt.savefig(saveloc)
     plt.close()
---
+++ test.py
@@ -0,0 +1,6 @@
+def add_a(a):
+	a+=1
+
+a = 1
+add_a(a)
+print a
